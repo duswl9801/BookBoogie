df8047ce63f0caeb7668753f31f09d82e86b7f7a
duswl9801 <duswl9801@naver.com>
2020-07-01 00:42:19 +0900

    막대 그래프 코드 이해하기 위해 한줄 씩 바꾸면서 주석 써 봄

--- app/src/main/java/com/reading_app/bookboogie/CalenderActivity.java
+++ app/src/main/java/com/reading_app/bookboogie/CalenderActivity.java
@@ -1,5 +1,6 @@
 package com.reading_app.bookboogie;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -10,9 +11,9 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CalenderActivity extends AppCompatActivity {
 
@@ -27,16 +28,17 @@ public class CalenderActivity extends AppCompatActivity {
         ArrayList NoOfEmp = new ArrayList();
 
         // 값 추가. x index와 그에 맞는 수치 넣는 코드.
-        NoOfEmp.add(new BarEntry(945f, 0));
-        NoOfEmp.add(new BarEntry(1040f, 1));
-        NoOfEmp.add(new BarEntry(1133f, 2));
-        NoOfEmp.add(new BarEntry(1240f, 3));
-        NoOfEmp.add(new BarEntry(1369f, 4));
-        NoOfEmp.add(new BarEntry(1487f, 5));
-        NoOfEmp.add(new BarEntry(1501f, 6));
-        NoOfEmp.add(new BarEntry(1645f, 7));
-        NoOfEmp.add(new BarEntry(1578f, 8));
-        NoOfEmp.add(new BarEntry(1695f, 9));
+        // 수치에 따라 그래프 값 범위 자동으로 바뀜.
+        NoOfEmp.add(new BarEntry(3, 0));
+        NoOfEmp.add(new BarEntry(2, 1));
+        NoOfEmp.add(new BarEntry(1, 2));
+        NoOfEmp.add(new BarEntry(5, 3));
+        NoOfEmp.add(new BarEntry(0, 4));
+        NoOfEmp.add(new BarEntry(1, 5));
+        NoOfEmp.add(new BarEntry(2, 6));
+        NoOfEmp.add(new BarEntry(0, 7));
+        NoOfEmp.add(new BarEntry(0, 8));
+        NoOfEmp.add(new BarEntry(1, 9));
 
         // x축에 달 넣기 위한 어레이리스트
         ArrayList month = new ArrayList();
@@ -51,11 +53,30 @@ public class CalenderActivity extends AppCompatActivity {
         month.add("8");
         month.add("9");
         month.add("10");
+        month.add("11");
+        month.add("12");
 
-        BarDataSet bar_data_set = new BarDataSet(NoOfEmp, "No Od Empoloyee");
-        read_book_barchart.animateY(5000);
+        /*
+        차트로 나타낼 데이터.
+        label은 밑에 표시됨.
+         */
+        BarDataSet bar_data_set = new BarDataSet(NoOfEmp, "읽은 책들의 수");
+
+        /*
+          그래프 나타나는 속도 조절하는 코드.
+          animateY는 y축으로 위로 올라와지는 메소드고
+          animateX는 x축마다 하나씩 나옴
+         */
+        read_book_barchart.animateY(2000);
+
+        /*
+         x축과 데이터 셋을 묶어서 바 데이터에 넣음.
+         */
         BarData data = new BarData(month, bar_data_set);
-        bar_data_set.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        bar_data_set.setColors(Collections.singletonList(Color.DKGRAY));      // 막대 색 조절하는 코드
+
+        // 막대 그래프에 데이터 넣음.
         read_book_barchart.setData(data);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
